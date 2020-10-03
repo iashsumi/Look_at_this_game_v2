fb0236bdef16cef7a1254b6b5d94a2ac845eea87
s.tomoki <tanajammw@gmail.com>
2020-10-03 13:08:44 +0900

    [#1] add namespace api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :commentators, only: [:index, :show]
-  resources :videos, only: [:index]
-  resources :sc_threads, only: [:index, :show]
-  resources :dashboard, only: [:index]
-
+  scope :api do
+    resources :commentators, only: [:index, :show]
+    resources :videos, only: [:index]
+    resources :sc_threads, only: [:index, :show]
+    resources :dashboard, only: [:index]  
+  end
 end
