85e2d864514a0156c6af3494129c8044bac0b5a8
s.tomoki <tanajammw@gmail.com>
2020-10-18 21:57:58 +0900

    [#10]勢いの計算に小数点以下を考慮

--- lib/tasks/board/service.rb
+++ lib/tasks/board/service.rb
@@ -54,8 +54,8 @@ class Tasks::Board::Service
       next if item.thread_created_at.blank?
 
       # レス数 / (現在のUNIX時間 - スレッド内の1番目の投稿のUNIX時間) ÷ 86400
-      num = (Time.now.to_i - item.thread_created_at.to_i) / 86_400
-      item.momentum = num.zero? ? 0 : item.res / num
+      num = ((Time.now.to_i - item.thread_created_at.to_i) / 86_400).to_f
+      item.momentum = num.zero? ? 0 : (item.res / num).to_f
       target << item
     end
     ScThread.import(target, on_duplicate_key_update: [:momentum])
