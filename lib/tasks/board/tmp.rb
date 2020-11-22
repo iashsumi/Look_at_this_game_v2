c108272b6336f05b533dd70b9c257ca7f6245529
s.tomoki <tanajammw@gmail.com>
2020-11-22 22:55:53 +0900

    一時バッチ修正

--- lib/tasks/board/tmp.rb
+++ lib/tasks/board/tmp.rb
@@ -3,7 +3,7 @@
 # docker-compose exec app bundle exec rails runner Tasks::Board::Tmp.rescue_execute
 class Tasks::Board::Tmp < Tasks::Base
   def self.execute
-     traget = ScThread.where(is_backup: true, is_completed: false)
+     traget = ScThread.where(is_backup: true, is_completed: false).limit(180)
      Tasks::Board::Exec.execute(traget)
   end
 end
