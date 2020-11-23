f9cffb2922a6c3bc0f079ae61ec346abcb84f789
s.tomoki <tanajammw@gmail.com>
2020-11-23 18:33:32 +0900

    デプロイ先追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@ set :rails_env, "production"
 
 # サーバー、ユーザー、ロールの指定
 server Rails.application.credentials.app_host, user: "ec2-user", roles: %w{app db web}
+server Rails.application.credentials.batch_host, user: "ec2-user", roles: %w{app db web}
 
 # デプロイ先のリポジトリ指定(/home/ec2-user/test-deploy.git)
 set :repo_url, "/home/ec2-user/deploy.git"
