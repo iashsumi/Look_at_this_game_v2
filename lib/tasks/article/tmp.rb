8302d99cc9b6584cd48df1ee39b8866654efe2d5
s.tomoki <tanajammw@gmail.com>
2020-11-23 14:51:13 +0900

    修正

--- lib/tasks/article/tmp.rb
+++ lib/tasks/article/tmp.rb
@@ -4,7 +4,7 @@
 class Tasks::Article::Tmp < Tasks::Base
   def self.execute
     limit = Configuration.all.first.value.to_i
-    target = ScThreadKeyword.eager_load(:sc_thread).merge(ScThread.labeling).merge(ScThread.where(is_completed: true, is_backup: true).where.not(id: Article.all.pluck(:sc_thread_id))).limit(limit)
+    target = ScThreadKeyword.eager_load(:sc_thread).merge(ScThread.labeling).merge(ScThread.where(is_completed: true, is_backup: true).where.not(id: Article.all.pluck(:sc_thread_id).uniq)).limit(limit)
     Tasks::Article::Create.execute(nil, nil, target)
   end
 end
